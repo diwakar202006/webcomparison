5b0f645a2f3c43aee5f8cf28ac2ca11214bbc163
diwakar202006 <diwakarsai202006@gmail.com>
2026-03-10 22:19:19 +0530

    commit

--- scraper.py
+++ scraper.py
@@ -1,6 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
 def compare_price(product):
 
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    # AMAZON SEARCH
+    amazon_url = f"https://www.amazon.in/s?k={product}"
+
+    r = requests.get(amazon_url, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    price = soup.select_one(".a-price-whole")
+
+    if price:
+        amazon_price = price.text
+    else:
+        amazon_price = "Not found"
+
     return {
-        "amazon": "₹52,999",
-        "flipkart": "₹51,499"
+        "amazon": amazon_price,
+        "flipkart": "Coming soon"
     }
